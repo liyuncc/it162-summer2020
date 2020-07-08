c2a8a7f876654cc4d96af3c0068375ad82feea87
Liyun Cecil <liyuncecil@gmail.com>
2020-07-07 17:57:40 -0700

    Upload of initial project files
    Adds index.php and images folder

---
+++ it162/index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>Liyun's Portal Page</title> 
+<meta charset="UTF-8">
+<style>
+    
+    
+    body {
+        background-image: url(images/background.jpg);  
+    }
+    
+    
+    h1, h2, h3, h4 {
+        margin-bottom: 7px;
+    }
+    
+    p {
+        margin-bottom: 20px;
+        
+    }
+    
+    ul, ol {
+        margin: 0 0 20px 30px;
+    }
+    
+    div#wrapper {
+        width: 940px;
+        margin: 0 auto;
+    }
+    
+    img {
+        float: right;
+        border: 5px #0099ff solid;
+        
+    }
+    
+    
+</style>
+</head>
+
+<body>
+    
+<div id="wrapper">
+<h1>Welcome to Liyun's Portal Page</h1>    
+<img src="images/liyun.jpg" alt="Picture of Liyun">
+    
+<h2>A Little About Me</h2>
+<!-- Self introduction-->
+<p>My name is Liyun Cecil. I am from Taiwan and currently live in Seattle with two boys and my husband. I was practicing music therapy for several years before going back to school. I love playing the piano and making music with my family and friends. I also love the sea creatures in Pacific Northwest. After learning more about the plastic pollution in our oceans, I am now an advocator for <strong>plastic-free oceans</strong>!!</p>
+    
+<h2>My Passion and Professional Goals</h2>
+<!-- What is my passion?-->
+<p>It all began with a few conversations with a friend working in the bioinformatics field. I was fascinated with how his work combines computer science and biology to help patients. He writes programs using genetic data from children with asthma to discover genes associated with asthma. His results provide potential advancements to preventative medicine. His work inspired me to learn more about programming and its applications in medicine. So here I am! My <em>short-term goal</em> is to complete certificates in programming and web development in summer 2020. My <em>five-year goal</em> is to be a software developer in the medical field, working to improve people’s lives alongside other healthcare professionals.</p>
+
+<hr>
+
+<h3>Assignments</h3>
+<ul>
+
+
+</ul>
+
+<hr>
+    
+<p><b><a href="https://validator.w3.org/check?uri=referer">Valid!</a></b></p>    
+    
+</div>
+</body>
+
+
+</html>
