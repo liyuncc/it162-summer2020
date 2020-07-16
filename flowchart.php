dcd9dacc9e7e51c9e782289dc586e79240221570
Liyun Cecil <liyuncecil@gmail.com>
2020-07-16 13:50:49 -0700

    Uploads 5 files of Liyun's Portal Website

---
+++ flowchart.php
@@ -0,0 +1,22 @@
+<?php include "includes/header.php"; ?>
+
+<!--<h1>Flowchart</h1>-->
+
+
+</section>
+<!-- END LEFT COL-->
+    
+    
+<!-- START RIGHT COL-->
+<aside>
+    <h2>Right Column</h2>
+    <!-- Self introduction-->
+    <p>Lollipop liquorice toffee toffee croissant soufflé croissant gummi bears. Chocolate cake muffin marshmallow cake sesame snaps. Icing jelly-o powder.</p>
+
+    <p>Bear claw danish croissant gingerbread. Tootsie roll cookie marzipan ice cream tiramisu gummies jujubes gummi bears. Tiramisu sweet roll lollipop chocolate cake pudding. Dessert topping pastry halvah sweet pastry tiramisu sugar plum dragée.</p>
+  
+    
+</aside>
+<!-- END RIGHT COL-->
+    
+<?php include "includes/footer.php"; ?>
--- it162/index.php
+++ it162/index.php
@@ -1,72 +1,25 @@
-<!doctype html>
-<html lang="en">
-<head>
-<title>Liyun's Portal Page</title> 
-<meta charset="UTF-8">
-<style>
-    
-    
-    body {
-        background-image: url(images/background.jpg);  
-    }
-    
-    
-    h1, h2, h3, h4 {
-        margin-bottom: 7px;
-    }
-    
-    p {
-        margin-bottom: 20px;
-        
-    }
-    
-    ul, ol {
-        margin: 0 0 20px 30px;
-    }
-    
-    div#wrapper {
-        width: 940px;
-        margin: 0 auto;
-    }
-    
-    img {
-        float: right;
-        border: 5px #0099ff solid;
-        
-    }
-    
-    
-</style>
-</head>
-
-<body>
-    
-<div id="wrapper">
-<h1>Welcome to Liyun's Portal Page</h1>    
-<img src="images/liyun.jpg" alt="Picture of Liyun">
-    
-<h2>A Little About Me</h2>
-<!-- Self introduction-->
-<p>My name is Liyun Cecil. I am from Taiwan and currently live in Seattle with two boys and my husband. I was practicing music therapy for several years before going back to school. I love playing the piano and making music with my family and friends. I also love the sea creatures in Pacific Northwest. After learning more about the plastic pollution in our oceans, I am now an advocator for <strong>plastic-free oceans</strong>!!</p>
-    
-<h2>My Passion and Professional Goals</h2>
-<!-- What is my passion?-->
-<p>It all began with a few conversations with a friend working in the bioinformatics field. I was fascinated with how his work combines computer science and biology to help patients. He writes programs using genetic data from children with asthma to discover genes associated with asthma. His results provide potential advancements to preventative medicine. His work inspired me to learn more about programming and its applications in medicine. So here I am! My <em>short-term goal</em> is to complete certificates in programming and web development in summer 2020. My <em>five-year goal</em> is to be a software developer in the medical field, working to improve people’s lives alongside other healthcare professionals.</p>
-
-<hr>
-
-<h3>Assignments</h3>
-<ul>
+<?php include "includes/header.php"; ?>
 
 
-</ul>
-
-<hr>
+    <!-- 3 images here based on the screen size here-->
+    <img src="images/desktop.jpg" class="desktop" alt="Picture of Liyun">
+    <img src="images/phone.jpg" class="phone" alt="Picture of Liyun">
+    <img src="images/tablet.jpg" class="tablet" alt="Picture of sea turtle baby">
+</section>
+<!-- END LEFT COL-->
     
-<p><b><a href="https://validator.w3.org/check?uri=referer">Valid!</a></b></p>    
     
-</div>
-</body>
-
-
-</html>
+<!-- START RIGHT COL-->
+<aside>
+    <h2>A Little About Me</h2>
+    <!-- Self introduction-->
+    <p>My name is Liyun Cecil. I am from Taiwan and currently live in Seattle with my husband and two boys. I was practicing music therapy for several years before going back to school. I love playing the piano and making music with my family and friends. I also love the sea creatures in the Pacific Northwest. After learning more about the plastic pollution in our oceans, I am now an advocator for <strong>plastic-free oceans</strong>!!</p>
+  
+    <h3>My Passion and Professional Goals</h3>
+    <!-- What is my passion?-->
+    <p>It all began with a few conversations with a friend working in the bioinformatics field. I was fascinated with how his work combines computer science and biology to help patients. He writes programs using genetic data from children with asthma to discover genes associated with asthma. His results provide potential advancements to preventative medicine. His work inspired me to learn more about programming and its applications in medicine. So here I am!</p> 
+    <p>My <em>short-term goal</em> is to complete certificates in programming and web development at Seattle Central College in summer 2020. My <em>five-year goal</em> is to be a software developer in the medical field, working to improve people’s lives alongside other healthcare professionals.</p>
+</aside>
+<!-- END RIGHT COL-->
+    
+<?php include "includes/footer.php"; ?>
