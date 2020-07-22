33c2db558b691ade758bd25261097239e573b667
Liyun Cecil <liyuncecil@gmail.com>
2020-07-21 19:47:31 -0700

    Updates contact.php
    Adds contact form and reCAPTCHA v2

--- it162/contact.php
+++ it162/contact.php
@@ -1,21 +1,31 @@
 <?php include "includes/header.php"; ?>
 
-<!--<h1>Contact Me</h1>-->
-
+<!--contact page image here-->
+<img src="images/contact_big.jpg" alt="sea otter says hi" class="desktop">
+<img src="images/contact_tablet.jpg" alt="seal says hi" class="tablet">
+<img src="images/contact_phone.png" alt="sea turtle says hi" class="phone">
 
 </section>
 <!-- END LEFT COL-->
     
     
 <!-- START RIGHT COL-->
-<aside>
-    <h2>Right Column</h2>
-    <!-- Self introduction-->
-    <p>Lollipop liquorice toffee toffee croissant soufflé croissant gummi bears. Chocolate cake muffin marshmallow cake sesame snaps. Icing jelly-o powder.</p>
-
-    <p>Bear claw danish croissant gingerbread. Tootsie roll cookie marzipan ice cream tiramisu gummies jujubes gummi bears. Tiramisu sweet roll lollipop chocolate cake pudding. Dessert topping pastry halvah sweet pastry tiramisu sugar plum dragée.</p>
-  
+<aside id="contact_form">
+<!--    <h2>Drop me a line! </h2>-->
     
+    	<?php
+
+        include 'includes/contact_include.php'; #site keys & code here
+    
+        $toAddress = "liyuncecil@gmail.com";  //place your/your client's email address here
+        $toName = "Liyun Cecil"; //place your client's name here
+        $website = "Liyun's IT 162 Portal Contact Form";  //place NAME of your client's website
+
+//      echo loadContact('simple.php');#demonstrates a simple contact form
+        echo loadContact('multiple.php');#demonstrates multiple form elements
+
+	?>
+
 </aside>
 <!-- END RIGHT COL-->
     
