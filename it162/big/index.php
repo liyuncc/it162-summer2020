fb13e8f682a52111865e6bab951cfa4d7915f37c
Liyun Cecil <liyuncecil@gmail.com>
2020-08-15 21:45:32 -0700

    Updates index.php
    Adds navigation, menu maker and font awesome

--- it162/big/index.php
+++ it162/big/index.php
@@ -1,1 +1,20 @@
 <!--index.php of big project-->
+<?php include "includes/header.php"; ?> 
+    <p>Develop and place the form</p>
+    <form>add multiple.php form here</form>
+    
+    </div>
+    <!-- END LEFT COL-->
+
+
+    <!-- START RIGHT COL-->
+    <div class="rightcol">
+        <h3>Basic Website Design Cycle</h3>
+
+    </div>
+    <!-- END RIGHT COL-->
+   
+</div>
+<!-- END ROW WRAPPER-->
+
+<?php include "includes/footer.php"; ?>
