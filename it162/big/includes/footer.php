a89cbb95a34b5b7f08ab49be748b1dc8cfda359f
Liyun Cecil <liyuncecil@gmail.com>
2020-08-15 21:51:14 -0700

    Uploads includes folder
    Adds the config file, header.php, and footer.php

---
+++ it162/big/includes/footer.php
@@ -0,0 +1,15 @@
+<!-- START FOOTER-->
+<footer>
+    <p><small>&copy; 2020 - <?=date('Y')?> by <a href="contact.php" target="_blank">Liyun Cecil</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+</footer>
+<!-- END FOOTER-->
+  
+    
+    
+</div>
+<!-- END DIV WRAPPER-->
+    
+</body>
+
+
+</html>
---
+++ it162/big/includes/header.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Liyun Cecil's IT162 BIG Page</title>
+    <meta name="viewport" content="width=device-width">
+    <meta name="robots" content="noindex,nofollow">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/big.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/form.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
+    <script src="js/script.js"></script>
+    
+</head>
+
+<body>
+<!-- START WAPPER-->
+<div class="wrapper">
+    <header>
+    <!--<header> has the semantic meaning for the browser-->
+        <h1><a href="index.php"><i class="logo fa fa-home"></i>Liyun Cecil's SCC IT162 BIG Page</a></h1>    
+
+        <nav id="cssmenu"> 
+
+          <ul>
+             <li><a href="../index.php"><span><i class="fa fa-fw fa-bank"></i> IT162 Portal</span></a></li>
+             <li><a href="index.php"><span><i class="fa fa-fw fa-home"></i> BIG</span></a></li>
+             <li><a href="flexbox.php"><span><i class="fa fa-fw fa-cube"></i> Flexbox</span></a></li>
+             <li><a href="galleries.php"><span><i class="fa fa-fw fa-camera-retro"></i> Galleries</span></a></li>
+             <li><a href="#"><span><i class="fa fa-fw fa-chevron-down"></i> Google</span></a>
+                <ul>
+                   <li><a href="calendar.php"><span><i class="fa fa-fw fa-calendar"></i> Calendar</span></a></li>
+                   <li><a href="map.php"><span><i class="fa fa-fw fa-map-o"></i> Map</span></a></li>
+                   <li><a href="youtube.php"><span><i class="fa fa-fw fa-youtube-square"></i> YouTube</span></a></li>
+                </ul>
+             </li>
+             <li><a href="shoppingcarts.php"><span><i class="fa fa-fw fa-shopping-basket"></i> Shopping Carts</span></a></li>
+             <li><a href="siteapp.php"><span><i class="fa fa-fw fa-html5"></i> Site vs App</span></a></li>
+             <li><a href="webcam.php"><span><i class="fa fa-fw fa-eye"></i> Web Cam</span></a></li>
+          </ul>
+
+        </nav>
+
+    </header>  
+
+
+<div class="row">
+    <!-- START LEFT COL-->
+    <div class="leftcol">
+        <h2>Main Page</h2>
