fb2e5911b7e16d93132d685a4bdbe11ad5b0bcf4
Liyun Cecil <liyuncecil@gmail.com>
2020-08-15 21:43:39 -0700

    Updates big/index.php
    Adds navigation, CSS menu maker and font awesome

--- it162/index.php
+++ it162/index.php
@@ -1,25 +1,20 @@
-<?php include "includes/header.php"; ?>
+<!--index.php of big project-->
+<?php include "includes/header.php"; ?> 
+    <p>Develop and place the form</p>
+    <form>add multiple.php form here</form>
+    
+    </div>
+    <!-- END LEFT COL-->
 
 
-    <!-- 3 images here based on the screen size here-->
-    <img src="images/desktop.jpg" class="desktop" alt="Picture of Liyun">
-    <img src="images/phone.jpg" class="phone" alt="Picture of Liyun">
-    <img src="images/tablet.jpg" class="tablet" alt="Picture of sea turtle baby">
-</section>
-<!-- END LEFT COL-->
-    
-    
-<!-- START RIGHT COL-->
-<aside>
-    <h2>A Little About Me</h2>
-    <!-- Self introduction-->
-    <p>My name is Liyun Cecil. I am from Taiwan and currently live in Seattle with my husband and two boys. I was practicing music therapy for several years before going back to school. I love playing the piano and making music with my family and friends. I also love the sea creatures in the Pacific Northwest. After learning more about the plastic pollution in our oceans, I am now an advocator for <strong>plastic-free oceans</strong>!!</p>
-  
-    <h3>My Passion and Professional Goals</h3>
-    <!-- What is my passion?-->
-    <p>It all began with a few conversations with a friend working in the bioinformatics field. I was fascinated with how his work combines computer science and biology to help patients. He writes programs using genetic data from children with asthma to discover genes associated with asthma. His results provide potential advancements to preventative medicine. His work inspired me to learn more about programming and its applications in medicine. So here I am!</p> 
-    <p>My <em>short-term goal</em> is to complete certificates in programming and web development at Seattle Central College in summer 2020. My <em>five-year goal</em> is to be a software developer in the medical field, working to improve people’s lives alongside other healthcare professionals.</p>
-</aside>
-<!-- END RIGHT COL-->
-    
+    <!-- START RIGHT COL-->
+    <div class="rightcol">
+        <h3>Basic Website Design Cycle</h3>
+
+    </div>
+    <!-- END RIGHT COL-->
+   
+</div>
+<!-- END ROW WRAPPER-->
+
 <?php include "includes/footer.php"; ?>
