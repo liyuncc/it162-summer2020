88df0d239ebf339ef3101d1df29fc8d3f268079a
Liyun Cecil <liyuncecil@gmail.com>
2020-08-17 17:07:15 -0700

    Updates map.php
    Adds google map, address, and phone number

--- it162/big/map.php
+++ it162/big/map.php
@@ -1,6 +1,8 @@
 <!--index.php of big project-->
 <?php include "includes/header.php"; ?> 
-    <p>iFrame a responsive google map to the campus here</p>
+        <div class="embed-container">
+           <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.2389628133487!2d-122.35031854929476!3d47.62148599484141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54901546f87aca63%3A0xd1e929cd1149d812!2sMuseum%20of%20Pop%20Culture!5e0!3m2!1sen!2sus!4v1597687779992!5m2!1sen!2sus' allowfullscreen></iframe>
+        </div>
     
     
     </div>
@@ -10,9 +12,11 @@
     <!-- START RIGHT COL-->
     <div class="rightcol">
         <h3>Contact Info</h3>
-        <p>Address</p>
-        <p>Phone Number</p>
-
+        <address>
+            325 5th Ave N, <br>
+            Seattle, WA 98109
+        </address>
+        <a href="tel:12067702700">(206) 770-2700</a>
     </div>
     <!-- END RIGHT COL-->
    
