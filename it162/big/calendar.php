9d369b6b284d27581b1c2aa9c1fca04dc4251578
Liyun Cecil <liyuncecil@gmail.com>
2020-08-17 17:09:59 -0700

    Updates calendar.php
    Inserts google calendar: events with a clickable link and information

--- it162/big/calendar.php
+++ it162/big/calendar.php
@@ -1,16 +1,27 @@
 <!--index.php of big project-->
 <?php include "includes/header.php"; ?> 
-    <p>IFrame a responsive google calendar here and it should have one event on it that is clickale with a description</p>
-    
-    
+
+<!--Show/hide MONTH view calendar based on resolution-->
+        <div class='month-view embed-container'>
+            <iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=a21uNW9jODMxZ2pzNXRmbmM3aGgzcHB2aW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%238E24AA&showTitle=0&showNav=1&showPrint=0&showTabs=0&showCalendars=0'></iframe>
+        </div>
+
+    <!--Show/hide AGENDA view calendar based on resolution-->        
+        <div class='agenda-view embed-container'>
+            <iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=a21uNW9jODMxZ2pzNXRmbmM3aGgzcHB2aW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%238E24AA&showTitle=0&showPrint=0&showTabs=1&showCalendars=1&mode=AGENDA&showNav=1'></iframe>
+        </div>
     </div>
+
     <!-- END LEFT COL-->
 
 
     <!-- START RIGHT COL-->
     <div class="rightcol">
-        <h3>Event Info</h3>
-        <p>Event desctiption</p>
+        <h3>Upcoming Events</h3>
+        <h4>Festival: Tibet Fest at Seattle Cetner</h4>
+            <p>The Tibetan Association of Washington is excited to announce the 25th annual Tibet Fest on August 22nd and 23rd, 2020.</p>
+
+            <p>We’ll also be featuring conversations with contemporary Tibetan artists &amp; poets, a fun video highlighting Tibetan food plus a short program on textiles!</p>
 
     </div>
     <!-- END RIGHT COL-->
@@ -19,3 +30,4 @@
 <!-- END ROW WRAPPER-->
 
 <?php include "includes/footer.php"; ?>
+
