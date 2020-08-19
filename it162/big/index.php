c5ffc4c5bc248d87178e418a2167ae1a609b974c
Liyun Cecil <liyuncecil@gmail.com>
2020-08-18 21:29:05 -0700

    Updates index page of BIG
    Adds client questionnaire, reCaptcha, and web design cycle resources

--- it162/big/index.php
+++ it162/big/index.php
@@ -1,7 +1,16 @@
-<!--index.php of big project-->
-<?php include "includes/header.php"; ?> 
-    <p>Develop and place the form</p>
-    <form>add multiple.php form here</form>
+<?php include 'includes/header.php'; ?>
+        
+    <?php 
+        include 'includes/contact_include.php';
+        
+        $toAddress = "liyuncecil@gmail.com";  //place your/your client's email address here
+        $toName = "Liyun Cecil"; //place your client's name here
+        $website = "Liyun's IT162 BIG Client Questionnaire Form";  //place NAME of your client's website
+
+        echo loadContact('multiple.php');#demonstrates multiple form elements
+
+    ?>
+    
     
     </div>
     <!-- END LEFT COL-->
@@ -10,11 +19,21 @@
     <!-- START RIGHT COL-->
     <div class="rightcol">
         <h3>Basic Website Design Cycle</h3>
+        <img id="web-cycle" src="images/webdevcycle.png" alt="Web Design Life Cycle" title="Web Design Life Cycle">
+        <h4>More Resources:</h4>
+        <ul>
+            <li><a href="https://the-loupe.com/blog/7-phases-of-web-design-and-development-life-cycle" target="_blank">The Loupe</a></li>
+            <li><a href="https://webflow.com/blog/the-web-design-process-in-7-simple-steps" target="_blank">Webflow Blog</a></li>
+            <li><a href="https://xbsoftware.com/blog/website-development-process-full-guide/" target="_blank">XB Software</a></li>
+        </ul>
 
     </div>
     <!-- END RIGHT COL-->
    
-</div>
-<!-- END ROW WRAPPER-->
+</div>    
 
-<?php include "includes/footer.php"; ?>
+<?php include 'includes/footer.php'; ?>
+
+
+
+
