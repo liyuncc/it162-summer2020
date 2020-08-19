e5af3c19bb29065c7d1da1660fee5d5c77886dcb
Liyun Cecil <liyuncecil@gmail.com>
2020-08-18 21:40:36 -0700

    Uploads webcam.html
    Adds youtube and NYC DOT live webcams

--- it162/big/webcam.php
+++ it162/big/webcam.php
@@ -1,18 +1,24 @@
 <!--index.php of big project-->
 <?php include "includes/header.php"; ?> 
-<p>IFrame 2 different kinds of web cams.</p>
-<p>Left column will be a live youtube video. </p>
+    <h3>Watch NASA: Earth From Space</h3> 
+    <div class="embed-container">
+       
+        <div class="embed-container">
+            <iframe src="https://www.youtube.com/embed/DDU-rZs-Ic4" allowfullscreen></iframe>
+        </div>
     
     
     </div>
     <!-- END LEFT COL-->
-
+</div>
 
     <!-- START RIGHT COL-->
     <div class="rightcol">
-        <h3>DOT camera</h3>
-
-    </div>
+        <h3>DOT Web Cam: Brooklyn Bridge, NYC</h3>
+        <div class="dot">
+            <iframe src="http://www.liyuncecilcodes.com/it162/big/webcam.html" allowfullscreen></iframe>
+           
+        </div>
     <!-- END RIGHT COL-->
    
 </div>
