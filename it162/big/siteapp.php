753492694c25dc981b2c9bbf7027894e6e06f410
Liyun Cecil <liyuncecil@gmail.com>
2020-08-18 21:33:09 -0700

    Updates siteapp.php
    Adds content and citations

--- it162/big/siteapp.php
+++ it162/big/siteapp.php
@@ -1,6 +1,11 @@
 <!--index.php of big project-->
 <?php include "includes/header.php"; ?> 
-    <p>Copy and paste my responsive site vs mobile app research paper here</p>
+    <p>There are many discussions about responsive websites and mobile applications nowadays, especially for businesses. Why is that? Before we start, let’s have a basic understanding of responsive websites and mobile applications first.</p> 
+    <p>What are responsive websites? They are websites that look great on and are responsive to different devices. Web developers usually write the code once but make the sites responsive: the layout will change corresponding to screen sizes but the overall look and feel will stay the same.</p> 
+    <p>What are mobile applications? They are “a type of application software designed to run on a mobile device, such as a smartphone or tablet computer. Apps are generally small, individual software units with limited function (What is a Mobile Application? - Definition from Techopedia).”</p>
+    <p>This difference is a big topic because people’s behaviors are changing for accessing the internet. Before smartphones were introduced, people mainly used desktops or laptops to access the Internet. The population of mobile users is growing rapidly. According to Cherednichenko (2020), “The use of mobile devices is growing extremely fast.” In addition, global mobile traffic will grow from 11.51% in 2017 to 77.49% in 2022.</p> 
+    <p>Companies used to have a website as the main platform to interact with their customers. For example, making purchases, booking services, and so on. Also, websites play a crucial role in marketing and brand recognition. Now, more and more companies also have mobile applications to interact with customers in a different fashion. First of all, they provide the most responsive and fastest user experience (Mroczkowska, 2020). For example, we can open a workout app and start working out right away. Second, they utilize push notifications to bring back the audience’s attention and increase the frequencies to interact with them. Third, they have access to the microphone and the camera which is very convenient and interactive. These are unique features that responsive websites might lack. However, mobile apps are not perfect. “The most important drawback of native apps is their cost (Mroczkowska, 2020).” It costs more time and money to build and maintain an app compared to a website. Another downside is that users have to download and install the app before they can use it. Users can access the website without this extra step.</p>
+	<p>Which one is the best? There’s no right or wrong answer. According to Korol, “To choose one of them you must understand the purpose of the product: who will use it and how often.” Therefore, It depends on who the companies’ targeted populations are. With a better understanding of their behaviors and habits, companies will be able to make a cost-effective decision to interact with them and keep the business growing.</p>
     
     
     </div>
@@ -10,7 +15,12 @@
     <!-- START RIGHT COL-->
     <div class="rightcol">
         <h3>Citations</h3>
-
+            <ul>
+                <li>Cherednichenko, Sveta. <a href="https://www.mobindustry.net/mobile-app-responsive-website-or-something-in-between-what-to-choose-for-your-business-in-2019/" target="_blank">“Mobile App, Responsive Website, or Something in Between: What to Choose for Your Business.”</a> Mobindustry, 20 May 2020.</li>
+                <li>Mroczkowska, Agnieszka. <a href="https://www.thedroidsonroids.com/blog/what-is-a-mobile-app-app-development-basics-for-businesses" target="_blank">“What Is a Mobile App?: App Development Basics for Businesses.”</a> Droids On Roids, 23 July 2020.</li>
+                <li>Korol, Viktor. <a href="https://thinkmobiles.com/blog/responsive-website-vs-mobile-app/" target="_blank">“Responsive Website vs Mobile App: Comparison.”</a> Thinkmobiles.</li>
+                <li><a href="https://www.techopedia.com/definition/2953/mobile-application-mobile-app" target="_blank">“What Is a Mobile Application? - Definition from Techopedia.”</a> Techopedia.com.</li>
+            </ul>
     </div>
     <!-- END RIGHT COL-->
    
