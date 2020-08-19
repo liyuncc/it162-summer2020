04ef0178953ceb35393dc723ed4ada3b40781739
Liyun Cecil <liyuncecil@gmail.com>
2020-08-18 21:30:55 -0700

    Updates shoppingcarts.php
    Adds the content and its citations

--- it162/big/shoppingcarts.php
+++ it162/big/shoppingcarts.php
@@ -1,8 +1,26 @@
 <!--index.php of big project-->
 <?php include "includes/header.php"; ?> 
-    <p>Copy and paste my shopping carts research paper here</p>
+    <h4>What is an online shopping cart?</h4>
+    <p>An online shopping cart is “a piece of software that facilitates the purchase of a product or service. It accepts the customer's payment and organizes the distribution of that information to the merchant (Big Commerce).” It not only works as a virtual shopping cart that we can select and add items to it. It also acts several roles for our websites. For example, a product or service catalog with photos, description, and cost, the whole checkout process, as well as order and customer management. </p>
+    <p>There are mainly two types: hosted shopping carts and licensed shopping carts. A hosted shopping cart is hosted by a third-party company that “is responsible for server backups, maintenance, and upgrades (Big Commerce).” According to Hull, it is also called SaaS (Software as a service) which is easy to set up and we pay a monthly fee for the service, support, and security. It works great for small business owners or people who need more technical support. The downside is the limit of customization. A licensed shopping cart is highly customized which allows us to build our own based on our needs and requirements. However, the disadvantage of this type is time-consuming and a lot more difficult to operate. </p>
     
-    
+    <h4>Why is it so important?</h4>
+    <p>According to Big Commerce, shopping carts “bridge the gap between shopping and buying.” Therefore, having a shopping cart software that helps our business thrive and provides the best customer experience is critical.</p>
+
+    <h4>How to choose one that works best for our business?</h4>
+    <p>First of all, we need to be an expert in our business. How to achieve it? According to Rodriguez, we create a list of features that we absolutely need for our business. We also need to take our business’s size and rate of growth into consideration and make sure it is scalable in the future. In addition, Hull suggests writing down our main goals for our site because these goals will influence the type of platform we choose. After making both lists, Rodriguez highly recommends that we look at other websites for inspiration as well as study our competitor’s sites and analyze what features make them stand out in order to learn from them.</p>
+    <p>Next, platform evaluation. According to Rodriguez, it’s very important to compare our lists and the features that a platform offers side by side. We should take time evaluating the following features (Hull):</p>
+        <ol>
+            <li>The amount of storage space</li>
+            <li>Bandwidth</li>
+            <li>SEO</li>
+            <li>Store design and themes</li>
+            <li>Customer support</li>
+            <li>Security: fraud protection and Secure Socket Layout (SSL) encryption</li>
+            <li>Pricing</li>
+            <li>Other features: product reviews, wish lists, Q&amp;A, and social media</li>
+        </ol>
+    <p>After finishing the evaluation, it’s time for a test drive. It’s crucial to test a shopping cart’s ease of use to see if it’s user-friendly and meets all the essential criteria we’re looking for.</p>
     </div>
     <!-- END LEFT COL-->
 
@@ -10,7 +28,16 @@
     <!-- START RIGHT COL-->
     <div class="rightcol">
         <h3>Citations</h3>
-
+        <ul>
+            <li><a href="https://blog.payjunction.com/choosing-shopping-cart-software" target="_blank">“Guide to Choosing the Right Shopping Cart Software for Your Business.”</a> PayJunction Blog.</li>
+            <li><a href="https://dinarys.com/blog/how-to-choose-the-best-ecommerce-shopping-cart" target="_blank">“How to Choose the Best Ecommerce Shopping Cart.”</a> Dinarys.</li>
+            <li>Hull, Liz. <a href="https://www.merchantmaverick.com/shopping-carts-101-how-to-choose-a-shopping-cart-for-your-business/" target="_blank">“How To Choose The Perfect Shopping Cart For Your Business.”</a> Merchant Maverick, 13 Apr. 2020</li>
+            <li><a href="https://www.oberlo.com/ecommerce-wiki/shopping-cart" target="_blank">“What Is Shopping Cart?: Types of Shopping Carts - Learn More.”</a> Oberlo, Oberlo.</li>
+            <li><a href="https://www.bigcommerce.com/ecommerce-answers/whats-shopping-cart/" target="_blank">“What's an Ecommerce Shopping Cart? How to Optimize for More Money.”</a> BigCommerce, 12 Aug. 2020.</li>
+            <li><a href="https://www.top10ecommercesitebuilders.com/" target="_blank">“Www.top10ecommercesitebuilders.Com.”</a> Best Ecommerce Site Builders of 2020.</li>
+        
+        
+        </ul>
     </div>
     <!-- END RIGHT COL-->
    
