1c1531b98f4a777a20991a8a2af1c9dbf7528996
Liyun Cecil <liyuncecil@gmail.com>
2020-08-18 21:32:27 -0700

    Updates galleries.php
    Adds content and citations

--- it162/big/galleries.php
+++ it162/big/galleries.php
@@ -1,8 +1,20 @@
 <!--index.php of big project-->
 <?php include "includes/header.php"; ?> 
-    <p>Copy and paste my galleries research paper here</p>
-    
-    
+    <h4>Why do modern websites need image galleries? What’s the importance of it?</h4>
+    <p>There is a famous proverb saying “A picture is worth a thousand words.” According to Butler, a lot of studies and statistics have shown that images play a crucial role in communication. For example, “Facebook engagement is highest on posts with videos (13.9%) and photos (13.7%)” (Locowise, 2015). “Tweets with images receive 150% more retweets and 18% more clicks than tweets without (Buffer Social, 2016).” What we learn from these studies is that people pay great attention to images naturally. For most of us, when we are presented with information, we learn better with visual presentations than just text. We also have a great tendency to share images or videos when we want to connect with family and friends or capture people’s attention. </p>
+    <p>From a business’s point of view, “visual marketing is one of the most effective ways of advertising and promoting your business” (Hashemi). Having image galleries is the key way to achieve this goal. Galleries are examples of our work. They are a wonderful way to show our clients what services and skills we offer. They also attract our target audience. They will know that “our services are intended for them and they will be more inclined to explore our business further” (Hashemi). </p>
+    <p>Furthermore, Image galleries increase search engine optimization (SEO). According to Butler, websites will have a higher rank if the image matches the text when people do the image search. In addition, Hashemi pointed out that “35% of users will scroll through Google to find the image they are looking for, something that rarely occurs in the web content search.” </p>
+    <p>In conclusion, having an image or video gallery on our websites is essential for a successful business as well as making personal connections with our clients and attracting our target audience.</p>
+
+    <h4>What are the criteria for choosing galleries?</h4>
+    <p>According to Ivanovs, developers always seek the best ways to incorporate images or videos in web design for the best user experience. The author suggests that it’s very important to keep the following features in mind while looking for the right plugin: </p>
+        <ol>
+            <li>Flexibility and customization options</li>
+            <li>Lightweight codebase</li>
+            <li>The smoothness of content interaction</li>
+            <li>Responsive integration for all device and browser types</li>
+            <li>Overall design quality that reflects the best and most dynamic User Interface elements available</li>
+        </ol>
     </div>
     <!-- END LEFT COL-->
 
@@ -10,6 +22,12 @@
     <!-- START RIGHT COL-->
     <div class="rightcol">
         <h3>Citations</h3>
+        <ul>
+            <li>Hashemi, Chloe. <a href="https://www.techsling.com/importance-image-gallery-business-website/" target="_blank">“The Importance of an Image Gallery on a Business Website.”</a> TechSling Weblog, 26 Feb. 2016.</li>
+            <li><a href="http://www.thoughtmechanics.com/the-importance-of-images-in-web-design/" target="_blank">“The Importance of Images in Web Design.”</a> Thought Mechanics, 10 June 2015.</li>
+            <li>Ivanovs, Alex. <a href="https://colorlib.com/wp/jquery-image-gallery-sliders/" target="_blank">“Top 25 Free JQuery Image &amp; Gallery Sliders To Showcase Your Images 2019.”</a> Colorlib, 28 June 2019.</li>
+            <li>Butler, Mihaela Lica. <a href="https://www.carmelon-digital.com/articles/the-importance-of-images-on-a-website/" target="_blank">“The Importance of Images on a Website.”</a> Carmelon Digital Marketing, 2018.</li>    
+        </ul>
 
     </div>
     <!-- END RIGHT COL-->
